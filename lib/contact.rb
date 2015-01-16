b017fdab543ec761da74ddca4b4f1db991e4fd18
Tina Ramsey <tina.ramsey87@gmail.com>
2015-01-16 09:49:21 -0800

    list contacts and give each contact a unique id

--- lib/contact.rb
+++ lib/contact.rb
@@ -2,8 +2,29 @@ class Contact
 
   attr_reader(:name, :number)
 
+  @@contacts = []
+
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @number = attributes.fetch(:number)
+    @id = @@contacts.length().+(1)
   end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:list) do
+    @@contacts
+  end
+
+  define_method(:add) do
+    @@contacts.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@contacts = []
+  end
+
+
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,6 +3,11 @@ require('pry')
 require('contact')
 
 describe(Contact) do
+
+  before() do
+    Contact.clear()
+  end
+
   describe('#name') do
     it('returns the name of the contact') do
       test_person = Contact.new({:name => "Peter", :number => "9716451564"})
@@ -10,5 +15,41 @@ describe(Contact) do
     end
   end
 
-  
+  describe('#number') do
+    it('returns the number of the contact') do
+      test_person = Contact.new({:name => "Peter", :number => "9716451564"})
+      expect(test_person.number()).to(eq("9716451564"))
+    end
+  end
+
+  describe('#add') do
+    it('adds the contact to the list of contacts') do
+      test_person = Contact.new({:name => "Peter", :number => "9716451564"})
+      test_person.add()
+      expect(Contact.list()).to(eq([test_person]))
+    end
+  end
+
+  describe('#id') do
+    it('returns a contact by id number') do
+      test_person = Contact.new({:name => "Peter", :number => "9716451564"})
+      test_person.add()
+      expect(test_person.id()).to(eq(1))
+    end
+  end
+
+  describe('.list') do
+    it('holds saved contacts; is empty at first') do
+      expect(Contact.list()).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it('clears the list of saved contacts') do
+      test_person = Contact.new({:name => "Peter", :number => "9716451564"})
+      test_person.add()
+      Contact.clear()
+      expect(Contact.list()).to(eq([]))
+    end
+  end
 end
